c841fa84709a86a70bdf49bd33f7a704be576575
gilbertchrist95 <gilbertchrist95@gmail.com>
2017-05-09 11:00:04 +0700

    Add Login Activity

--- app/src/main/java/cilok/com/lapakjahit/FashionPriaActivity.java
+++ app/src/main/java/cilok/com/lapakjahit/FashionPriaActivity.java
@@ -1,6 +1,5 @@
 package cilok.com.lapakjahit;
 
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -8,11 +7,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import cilok.com.lapakjahit.tabs.SlidingTabLayout;
+import cilok.com.lapakjahit.Tabs.SlidingTabLayout;
 
 public class FashionPriaActivity extends AppCompatActivity {
 
