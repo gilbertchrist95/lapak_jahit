afa6b5ebc650c5d6c4104a7177cb0d6ccfe55271
gilbertchrist95 <gilbertchrist95@gmail.com>
2017-05-11 11:52:06 +0700

    Login setengah jalan

---
+++ app/src/main/java/cilok/com/lapakjahit/controller/UserLocalStore.java
@@ -0,0 +1,8 @@
+package cilok.com.lapakjahit.controller;
+
+/**
+ * Created by Gilbert on 5/11/2017.
+ */
+
+public class UserLocalStore {
+}
---
+++ app/src/main/java/cilok/com/lapakjahit/log/L.java
@@ -0,0 +1,22 @@
+package cilok.com.lapakjahit.log;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by Gilbert on 5/11/2017.
+ */
+
+public class L {
+    public static void m(String message) {
+        Log.d("LapakJahit", "" + message);
+    }
+
+    public static void t(Context context, String message) {
+        Toast.makeText(context, message + "", Toast.LENGTH_SHORT).show();
+    }
+    public static void T(Context context, String message) {
+        Toast.makeText(context, message + "", Toast.LENGTH_LONG).show();
+    }
+}
