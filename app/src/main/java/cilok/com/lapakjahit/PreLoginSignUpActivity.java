deb1febc4b60888c8a88e26269fbae7ba91e28b3
gilbertchrist95 <gilbertchrist95@gmail.com>
2017-05-11 12:25:01 +0700

    Layout Login Jadi

--- app/src/main/java/cilok/com/lapakjahit/PreLoginSignUpActivity.java
+++ app/src/main/java/cilok/com/lapakjahit/PreLoginSignUpActivity.java
@@ -9,5 +9,7 @@ public class PreLoginSignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_login_sign_up);
+
+
     }
 }
