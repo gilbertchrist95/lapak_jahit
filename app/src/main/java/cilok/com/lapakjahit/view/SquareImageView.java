cee042d12db2819f68563fb6099d1124858b8dd5
alhaura19 <haura19@gmail.com>
2017-05-28 20:20:42 +0700

    + square image

---
+++ app/src/main/java/cilok/com/lapakjahit/view/SquareImageView.java
@@ -0,0 +1,34 @@
+package cilok.com.lapakjahit.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by Alhaura on 26/05/2017.
+ */
+
+public class SquareImageView extends android.support.v7.widget.AppCompatImageView {
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth()); // Snap to width
+    }
+}
